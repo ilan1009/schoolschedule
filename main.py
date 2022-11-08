e4108c36f4381339926ab066458806767d187ce1
ilan1009 <ilankrumholz@gmail.com>
2022-11-08 11:37:00 +0200

    tes

--- main.py
+++ main.py
@@ -11,66 +11,49 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def get_stuff(home_room):
+def get_stuff(home_room_index):
     """Gets the schedule changes for specific class and return using SELENIUM WEB DRIVER"""
+    url = "https://www.alliancetlv.com/עדכוני-מערכת"  # url
 
-    ############################
-    #                          #
-    #         CRAWLING         #
-    #                          #
-    ############################
+    # home_room_index = home_room_index
 
-    url = "https://www.alliancetlv.com/עדכוני-מערכת"  # URL
-
-    # Chrome options
     options = Options()
     options.add_argument("--headless")
 
     # Launch web driver
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(url)
 
-    # Enter IFRAME element
     frame = WebDriverWait(driver, 10).until(
         ec.visibility_of_element_located((By.CSS_SELECTOR, 'div#comp-kg9mlfwo iframe')))
-
     driver.switch_to.frame(frame)
 
     # Select class from dropdown
     drp_class = Select(driver.find_element(By.CSS_SELECTOR, "#TimeTableView1_ClassesList"))
-    drp_class.select_by_visible_text(str(home_room))
+    drp_class.select_by_value(str(home_room_index))
 
-    # Select schedule and click it with JS
-    changes_button = driver.find_element(By.CSS_SELECTOR, "#TimeTableView1_btnChanges")
+    changes_button = driver.find_element(By.CSS_SELECTOR,
+                                         "#TimeTableView1_btnChanges")  # Select schedule tab
     changes_button.click()
 
-    # Get the text element hidden in the schedule tab
-    changes_txt = driver.find_element(By.CSS_SELECTOR, '#TimeTableView1_PlaceHolder > div')
+    changes_txt = driver.find_element(By.CSS_SELECTOR,
+                                      '#TimeTableView1_PlaceHolder > div')  # Get the text element
 
-    ############################
-    #                          #
-    #     TEXT FORMATTING      #
-    #                          #
-    ############################
-
-    if changes_txt.text == 'אין שינויים':  # If empty just print that and return
+    # print(changes_txt.text)
+    if changes_txt.text == 'אין שינויים':  # if this do this
         return "No schedule changes"
+    else:
+        tmpfile = open('tmpfile.txt', 'w', encoding="utf-8")
+        tmpfile.truncate(0)
+        tmpfile.write(changes_txt.text)
+        tmpfile.close()
 
-    # Some kind of hard to read text formatting ahead; not the most optimal way.
-
-    # Open the file, clear it and write new text
-    with open('vars/tmp_file.txt', 'w', encoding="utf-8") as tmp_file:
-        tmp_file.truncate(0)
-        tmp_file.write(changes_txt.text)
-        tmp_file.close()
-
-    # Open the files again and truncate
-    with open('vars/tmp_file.txt', 'r', encoding="utf-8") as tmp_file, \
-            open('vars/tmp_file_out.txt', 'w', encoding="utf-8") as tmp_file_out:
-        tmp_file_out.truncate()
+        tmpfile = open('tmpfile.txt', 'r', encoding="utf-8")
+        tmpfileout = open('tmpfileout.txt', 'w', encoding="utf-8")
+        tmpfileout.truncate()
         while 1:
             # Get next line from file
-            line = tmp_file.readline()
+            line = tmpfile.readline()
 
             # if line is empty
             # end of file is reached
@@ -79,32 +62,24 @@ def get_stuff(home_room):
             if 'ביטול שעור' in line:
                 # print("Line{}: {}".format(count, line.strip()))
                 for i in range(8):
-                    if 'שיעור ' + str(i) in line:
-                        tmp_file_out.write(f'Period {str(i)} cancelled! W\n')
-            elif 'הזזת שיעור' in line:
-                for i in range(8):
-                    if 'לשיעור ' + str(i) in line:
-                        lesson = line.split(' לשיעור')[0].split(', ')[2]
-                        tmp_file_out.write(f'Class "{lesson}" moved to period {str(i)}')
-            elif 'מילוי מקום' in line:
-                for i in range(8):
-                    if 'שיעור ' + str(i) in line:
-                        lesson = line.split(', ')[4]
-                        print(lesson)
-                        tmp_file_out.write(f'Period {str(i)} replaced with class "{lesson}"')
-
+                    if ('שיעור ' + str(i)) in line:
+                        # print(f'Period {str(i)} cancelled! W')
+                        tmpfileout.write(f'Period {str(i)} cancelled! W\n')
             else:
-                tmp_file_out.write(line)
+                tmpfileout.write(line)
 
-    # Read file and return it
-    with open('vars/tmp_file_out.txt', 'r+', encoding="utf-8") as tmp_file_out:
-        out = tmp_file_out.read()
-        tmp_file_out.truncate()
+        tmpfileout.close()
+        tmpfile.close()
 
-    return out
+        tmpfileout = open('tmpfileout.txt', 'r+', encoding="utf-8")
+        out = tmpfileout.read()
+        print(out)
+
+        tmpfileout.close()
+
+        return out
 
 
-# Some options
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -116,11 +91,12 @@ async def send_stuff():
 
     print('Sending daily morning message')
 
+    channel = bot.get_channel(1033324125843894283)  # set channel
     await bot.wait_until_ready()  # Make sure your guild cache is ready
 
-    t_1 = get_stuff('ט - 1')  # ט1
-    t_2 = get_stuff('ט - 2')  # ט2
-    t_3 = get_stuff('ט - 3')  # ט3
+    t_1 = get_stuff(17)  # ט1
+    t_2 = get_stuff(18)  # ט2
+    t_3 = get_stuff(19)  # ט3
 
     await channel.send('ט1:')
     await channel.send('```\n' + str(t_1) + '\n```')
@@ -139,46 +115,28 @@ async def on_ready():
     scheduler = AsyncIOScheduler()
     print('Scheduler initiated')
 
-    # Read file to get previously saved ID
-    with open('vars/morningid.txt', 'r') as channelfile:
-        # Issue a warning
-        if channelfile.readline() == '':
-            print('SET CHANNEL URGENTLY')
-        else:
-            global channel  # Set global variable
-            channelfile.seek(0)
-            channelid = channelfile.read()
-            channel = bot.get_channel(int(channelid))
-            print(channel.name)
-
     # Job that runs send_stuff every day at 7:00
-    scheduler.add_job(send_stuff, CronTrigger(day_of_week="0, 1, 2, 3, 4, 6", hour="7", minute="0", second="30"))
+    scheduler.add_job(send_stuff, CronTrigger(day_of_week="0, 1, 2, 3, 4, 6", hour="7", minute="0", second="0"))
     scheduler.start()  # Start
 
 
 @bot.command(name='send')
 async def send(message, arg1=0):
-    """A command, Listens for $send, where $ is the prefix. when run with the correct arguments it
-     will return the schedule changes of the selected class using the get_stuff() function."""
+    """A command, Listens for $send, where $ is the prefix. when run with the correct arguments it will return the
+    schedule changes of the selected class using the get_stuff() function."""
 
     print('Command send requested')
+
+    arg1index = 16 + arg1
 
     if arg1 == 0:
         await message.channel.send('Failure to provide arguments')
     else:
         await message.channel.send('ט' + str(arg1) + ':')
         loading_msg = await message.channel.send('Loading...')
-        await loading_msg.edit(content='\n```\n' + (str(get_stuff(f'ט - {arg1}'))) + '\n```')
+        await loading_msg.edit(content='\n```\n' + (str(get_stuff(arg1index))) + '\n```')
         print('Task completed successfully')
 
 
-@bot.command(name='setchannel')
-async def setchannel(ctx):
-    """Sets the morning message to send to this channel."""
-    with open('vars/morningid.txt', 'a+') as channelfile:
-        channelfile.truncate(0)
-        channelfile.write(str(ctx.channel.id))
-        print(ctx.channel.name)
-
-
-bot.run('MTAzMzE3NDc1NzQyMTYzMzU1Ng.G9DLiS.mv8VEZ_cnAx8P5mkqcYlCMutRTfHvodzQurqsE')
+toggle = False
+bot.run('MTAzMzE3NDc1NzQyMU1Ng.GGQtOy.J0hwwRR4XfIzEvrUVPvrRm5pcsMoAVF09pU2TY')
