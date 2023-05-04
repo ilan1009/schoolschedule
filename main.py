b5101da805225c2fda59bd766528aaed5b377572
ilan1009 <ilankrumholz@gmail.com>
2023-05-04 12:50:41 +0300

    everything

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+import datetime
 
 URL = "https://www.alliancetlv.com/עדכוני-מערכת"  # URL
 
@@ -21,24 +22,90 @@ driver = webdriver.Chrome(options=options)
 driver.get(URL)
 
 # Enter IFRAME element
-frame = WebDriverWait(driver, 10).until \
-    (ec.visibility_of_element_located((By.CSS_SELECTOR, 'div#comp-kg9mlfwo iframe')))
+frame = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div#comp-kg9mlfwo iframe')))
 
 driver.switch_to.frame(frame)
 
 
-async def get_stuff(home_room, driver, refresh_option=False):
-    """Gets the schedule changes for specific class and return using SELENIUM WEB DRIVER"""
-    ############################
-    #                          #
-    #         CRAWLING         #
-    #                          #
-    ############################
+async def todayIs():
+    # Get current date
+    today = datetime.date.today()
 
-    if refresh_option:
-        driver.navigate().refresh()
+    # Get ISO calendar tuple (year, week number, day number)
+    iso = today.isocalendar()
+
+    # Get day of the week (Monday is 0, Sunday is 6)
+    day = iso[2] - 1
+
+    # Adjust for Sunday as the first day of the week
+    day = (day + 1) % 7
+
+    return day
+
+
+async def get_table_schedule(home_room, driver, day):
+    """Gets the schedule changes for specific class and return using SELENIUM WEB DRIVER"""
+    if day < 0:
+        day = await todayIs() + 1
+
     # Select class from dropdown
-    drp_class = Select(driver.find_element(By.CSS_SELECTOR, "#TimeTableView1_ClassesList"))
+    drp_class = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, '#TimeTableView1_ClassesList')))
+    drp_class = Select(drp_class)
+    drp_class.select_by_visible_text(str(home_room))
+
+    # Select schedule TABLE and click it with JS
+    changes_button = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, '#TimeTableView1_btnChangesTable')))
+    changes_button.click()
+
+    rowsHolder = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, '.TTTable > tbody:nth-child(1)')))
+
+    rows = rowsHolder.find_elements(By.XPATH, '*')
+    rows.pop(0)
+
+    hours = []
+    for row in rows:
+        # print(row.get_attribute("outerHTML"))
+        cells = row.find_elements(By.CSS_SELECTOR, '.TTCell')
+        hours.append(cells[day - 1])
+        # print(cells[day-1].get_attribute("outerHTML"))
+
+    hourSchedules = []
+    for hour in hours:
+        hourLessons = []
+        try:
+            hourLessonElements = hour.find_elements(By.CSS_SELECTOR, 'td.TableEventChange')
+            if hourLessonElements:
+                for hourLesson in hourLessonElements:
+                    hourLessons.append(hourLesson.text)
+
+            hourLessonsElements = hour.find_elements(By.CSS_SELECTOR, '.TTLesson')
+            if hourLessonsElements:
+                for hourLesson in hourLessonsElements:
+                    hourLessons.append(hourLesson.text)
+
+            if hourLessons:
+                hourSchedules.append(hourLessons)
+
+        except Exception as e:
+            continue
+
+    out = []
+    for i, j in enumerate(hourSchedules):
+        jjoined = '\n'.join(j)
+        out.append(f'({i + 1}) | {jjoined}')
+    return "\n------------------------------------------\n".join(out)
+
+
+async def get_stuff(home_room, driver):
+    """Gets the schedule changes for specific class and return using SELENIUM WEB DRIVER"""
+
+    # Select class from dropdown
+    drp_class = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, '#TimeTableView1_ClassesList')))
+    drp_class = Select(drp_class)
     drp_class.select_by_visible_text(str(home_room))
 
     # Select schedule and click it with JS
@@ -50,52 +117,37 @@ async def get_stuff(home_room, driver, refresh_option=False):
     changes_txt = WebDriverWait(driver, 10).until(
         ec.visibility_of_element_located((By.CSS_SELECTOR, '#TimeTableView1_PlaceHolder')))
 
-    ############################
-    #                          #
-    #     TEXT FORMATTING      #
-    #                          #
-    ############################
+    return formatRawSchedule(changes_txt.text)
 
-    # Some kind of hard to read text formatting ahead; not the most optimal way.
 
-    # Open the file, clear it and write new text
-    with open('vars/tmp_file.txt', 'r+', encoding="utf-8") as tmp_file:
-        tmp_file.truncate(0)
-        tmp_file.write(changes_txt.text)
+def formatRawSchedule(rawschedule):
+    if rawschedule == "אין שינויים":  # if empty
+        return "אין שינויים, לך תבכה אבל אל תשכח, יש לימודים ב8:15"
+    data = []
+    for i in range(8):
+        data.append(' \n')
+    for line in rawschedule.split('\n'):
+        if 'ביטול שעור' in line:
+            for i in range(8):
+                if 'שיעור ' + str(i) in line:
+                    data[i - 1] = f'Period {str(i)} cancelled! W\n'
+        elif 'הזזת שיעור' in line:
+            for i in range(8):
+                if 'לשיעור ' + str(i) in line:
+                    lesson = line.split(' לשיעור')[0].split(', ')[2]
+                    data[i - 1] = f'Class "{lesson}" moved to period {str(i)}\n'
 
-    with open('vars/tmp_file.txt', 'r+', encoding="utf-8") as tmp_file:
-        data = []
-        for i in range(8):
-            data.append(' \n')
-        while 1:
-            # Get next line from file
-            line = tmp_file.readline()
+        elif 'מילוי מקום' in line:
+            for i in range(8):
+                if 'שיעור ' + str(i) in line:
+                    lesson = line.split(', ')[4]
+                    print(lesson)
+                    data[i - 1] = f'Period {str(i)} replaced with class "{lesson}"\n'
 
-            # if line is empty
-            # end of file is reached
-            if not line:
-                break
-            if 'ביטול שעור' in line:
-                for i in range(8):
-                    if 'שיעור ' + str(i) in line:
-                        data[i - 1] = f'Period {str(i)} cancelled! W\n'
-            elif 'הזזת שיעור' in line:
-                for i in range(8):
-                    if 'לשיעור ' + str(i) in line:
-                        lesson = line.split(' לשיעור')[0].split(', ')[2]
-                        data[i - 1] = f'Class "{lesson}" moved to period {str(i)}\n'
-
-            elif 'מילוי מקום' in line:
-                for i in range(8):
-                    if 'שיעור ' + str(i) in line:
-                        lesson = line.split(', ')[4]
-                        print(lesson)
-                        data[i - 1] = f'Period {str(i)} replaced with class "{lesson}"\n'
-
-            elif 'החלפת חדר' in line:
-                for i in range(8):
-                    if 'שיעור ' + str(i) in line:
-                        data[i - 1] = line
+        elif 'החלפת חדר' in line:
+            for i in range(8):
+                if 'שיעור ' + str(i) in line:
+                    data[i - 1] = line
     out = ""
     return out.join(data)
 
@@ -114,16 +166,14 @@ async def send_stuff():
 
     await bot.wait_until_ready()  # Make sure your guild cache is ready
 
-    t_1 = await get_stuff('ט - 1', driver)  # ט1
-    t_2 = await get_stuff('ט - 2', driver)  # ט2
-    t_3 = await get_stuff('ט - 3', driver)  # ט3
+    classes = []
+    for i in range(0, 7):
+        c_i = await get_table_schedule('ט - ' + str(i + 1), driver)
+        classes.append(c_i)
 
-    await channel.send('ט1:')
-    await channel.send('```\n' + str(t_1) + '\n```')
-    await channel.send('ט2:')
-    await channel.send('```\n' + str(t_2) + '\n```')
-    await channel.send('ט3:')
-    await channel.send('```\n' + str(t_3) + '\n```')
+    for i in range(0, len(classes)):
+        await channel.send(
+            ('ט - ' + str(i + 1)) + classes[i] + '\n------------------------------------------\n..ilan -//h...')
 
 
 @bot.event
@@ -145,27 +195,81 @@ async def on_ready():
             channelfile.seek(0)
             channelid = channelfile.read()
             channel = bot.get_channel(int(channelid))
+            if not channel:
+                pass
             print(channel.name)
 
+    # await send_stuff()
     # Job that runs send_stuff every day at 7:00
     scheduler.add_job(send_stuff, CronTrigger(day_of_week="0, 1, 2, 3, 4, 6", hour="6", minute="50", second="0"))
     scheduler.start()  # Start
 
 
 @bot.command(name='send')
-async def send(message, arg1=0):
+async def send(message, arg1, arg2=-1):
     """A command, Listens for $send, where $ is the prefix. when run with the correct arguments it
      will return the schedule changes of the selected class using the get_stuff() function."""
 
     print('Command send requested')
 
-    if arg1 == 0:
+    if not arg1:
         await message.channel.send('Failure to provide arguments')
+
+    try:
+        arg1 = int(arg1)
+        if arg1 < 1 or arg1 > 7:
+            raise ValueError
+    except ValueError:
+        await message.channel.send(
+            'Invalid argument. Please provide an integer between 1 and 7 corresponding to classroom number\n'
+            'for example, 4 => ט-4.')
+        return
+    if arg2 != -1:
+        arg2_v = f"day {arg2}"
+        try:
+            arg2 = int(arg2)
+            if arg2 < 1 or arg2 > 5:
+                raise ValueError
+        except ValueError:
+            await message.channel.send(
+                'Invalid argument. Please provide an integer between 1 and 5 corresponding to the days sunday to thursday')
+            return
     else:
-        await message.channel.send('ט' + str(arg1) + ':')
+        await message.channel.send('Day not specified, sending todays schedule.')
+        arg2_v = "today"
+
+    await message.channel.send(f"Class {arg1}, {arg2_v}")
+    loading_msg = await message.channel.send('Loading...')
+    g = await get_table_schedule(f'ט - {arg1}', driver, arg2)
+    await loading_msg.edit(content=(str(g) + '\n------------------------------------------\n..ilan -//h...'))
+    print('Task completed successfully')
+
+
+@bot.command(name='sendold')
+async def sendold(message, arg1):
+    """A command, Listens for $send, where $ is the prefix. when run with the correct arguments it
+     will return the schedule changes of the selected class using the get_stuff() function."""
+
+    print('Command send requested')
+
+    if not arg1:
+        await message.channel.send('Failure to provide arguments')
+
+    try:
+        arg1 = int(arg1)
+        if arg1 < 1 or arg1 > 7:
+            raise ValueError
+    except ValueError:
+        await message.channel.send(
+            'Invalid argument. Please provide an integer between 1 and 7 corresponding to classroom number\n'
+            'for example, 4 => ט-4.')
+        return
+
+    else:
+        await message.channel.send('ט' + str(int(arg1)) + ':')
         loading_msg = await message.channel.send('Loading...')
         g = await get_stuff(f'ט - {arg1}', driver)
-        await loading_msg.edit(content='\n```\n' + (str(g)) + '\n```')
+        await loading_msg.edit(content=(str(g)))
         print('Task completed successfully')
 
 
